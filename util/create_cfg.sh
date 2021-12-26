a0994d3144279cc2cb6d8f9afb518af7e836fe97
Gennady Koshkin <gkeep@gkeep.xyz>
2021-12-26 14:46:17 +0300

    Fix config copying

--- util/create_cfg.sh
+++ util/create_cfg.sh
@@ -6,7 +6,7 @@ normal=$(tput sgr0)
 if [ ! -f "$XDG_CONFIG_HOME/spotify-stats/config" ]; then
   printf "${bold}installing config...${normal}\n"
   mkdir -p "$XDG_CONFIG_HOME/spotify-stats"
-  cp .env "$XDG_CONFIG_HOME/spotify-stats/config"
+  cp config.sample "$XDG_CONFIG_HOME/spotify-stats/config"
 else
   printf "${bold}config is already installed.${normal}\n"
 fi
